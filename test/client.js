d07545ecf96f700c053c3576833dbaf53ff644aa
Chr1s <32487758+Chr1sDev@users.noreply.github.com>
2022-01-07 00:54:55 -0600

    Update client.js

--- test/client.js
+++ test/client.js
@@ -2,7 +2,7 @@
   const { Client } = require("../src");
 
   const client = new Client(
-    "BQCR0IJUid4wdr_OhHS3O_ds__lmeICLrTexhrot_GhlejtQuMCIoY522b3QJZTip0h712Un4lBWbCGjDTtk8tZBhJuqe0cW7ntthqYnbF8Mn24BBr6ZIuYeEH1KDyRc3cSO4Sqy2vs06ptIlfIi7CsEwX7YkCaUDGUcLWtgDlzwBfSGdipt6dIvkRltkni8Y_sBDyFXzKMBIn6WjT_HOBeJRAr1aWaq7GjdMRxiYp69xPw3gb2-Umn3"
+    "[Redacted]"
   );
   console.log(await client.rest.request("GET", "/me"));
 })();
